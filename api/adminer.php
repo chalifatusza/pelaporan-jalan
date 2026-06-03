90298712f7e0e1f7f977c20d64f99c6235fa8da0
chalifatusza <chalifatuszahro@gmail.com>
2026-06-04 00:00:00 +0700

    Fix Adminer session expired issue by changing session save path to /tmp on Vercel

--- api/adminer.php
+++ api/adminer.php
@@ -1,4 +1,9 @@
 <?php
 
+// Fix session save path for Vercel read-only filesystem
+if (isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL'])) {
+    session_save_path('/tmp');
+}
+
 // Bridge for Adminer
 require __DIR__ . '/../backend/public/adminer.php';
